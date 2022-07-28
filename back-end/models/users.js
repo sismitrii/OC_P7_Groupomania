a369206a0f9e16cdb3fd232b285de0bb5922753f
sismitrii <floguerin156@gmail.com>
2022-07-28 11:41:28 +0200

    🚧 [model users] : 1st shot of model users

---
+++ back-end/models/users.js
@@ -0,0 +1,31 @@
+/*=============================================================*/
+/*------------------------ IMPORT -----------------------------*/
+/*=============================================================*/
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+/*=============================================================*/
+/*------------------------ Schema -----------------------------*/
+/*=============================================================*/
+
+function toLower(mail) { 
+    return mail.toLowerCase();
+  }
+
+const userSchema = mongoose.Schema({
+    email : {type : String, required : true, unique :true, set : toLower }, //if no set an email can have 2 acount flo@gmail.com and fLo@gmail.com are the same email but can be registred 1 time each
+    password : {type : String, required : true},
+    adminLvl : {type : Number, required, default : 3},
+    //subordinateList : {type : Array}
+    // admin level ?
+        // lvl 1 delete all publication, all comments, desactivated an user, and everything other lvl can do
+        // lvl 2 "responsable" can add to to do list of there "subordinate"
+        // lvl 3 normal actions, CRUD on there own publication, comment, task, profile
+})
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', userSchema)
+
+//https://mongoosejs.com/docs/api/schematype.html#schematype_SchemaType
+// https://www.npmjs.com/package/mongoose-unique-validator
