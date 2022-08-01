0ac30760444be4567eda352aba72dcbf05b80d71
sismitrii <floguerin156@gmail.com>
2022-08-01 15:56:46 +0200

    ✨ [comment] : deleteComment done with check of author 🐛 [comment] : modifyComment corrected now check author and authorisation before modification

--- back-end/controllers/comment.js
+++ back-end/controllers/comment.js
@@ -2,6 +2,7 @@
 /*------------------------ IMPORT -----------------------------*/
 /*=============================================================*/
 
+const User = require('../models/users')
 const Publication = require('../models/publications')
 const Comment = require('../models/comments')
 const auth = require('../middleware/auth')
@@ -36,12 +37,52 @@ exports.getCommentsOfPublication = (req, res, next)=>{
 
 
 exports.modifyComment = (req, res, next)=>{
-    Comment.findByIdAndUpdate(req.params.com_id, {$set : {content : req.body.content}})
-    .then(()=> res.status(201).json({message : "comment updated"}))
-    .catch((error) => res.status(400).json({message : "Error updating comment", error : error}))
+    User.findById(req.auth.userId)
+    .then((user)=>{
+        Comment.findById(req.params.com_id)
+        .then((comment)=>{
+            if (comment){
+                if ((comment.author === req.auth.userId) || (user.role.includes("ROLE_ADMIN"))){
+                        Comment.findByIdAndUpdate(req.params.com_id, {$set : {content : req.body.content}})
+                        .then(()=> res.status(201).json({message : "comment updated"}))
+                        .catch((error) => res.status(400).json({message : "Error updating comment", error : error}))
+                } else {
+                    res.status(403).json({message : "Unauthorized"})
+                }
+            } else {
+                res.status(200).json({message : "This comment doesn't exist"})
+            }
+        })
+        .catch((error)=> res.status(400).json({message : "Error find comment to modify", error : error}))
+    })
+    .catch((error)=> res.status(400).json({message : "Error find User to check role", error : error}))
 }
 
 
 exports.deleteComment = (req, res, next)=>{
-    
+    User.findById(req.auth.userId)
+    .then((user)=>{
+        Comment.findById(req.params.com_id)
+        .then((comment)=>{
+            if(comment){
+                if ((comment.author === req.auth.userId) || (user.role.includes("ROLE_ADMIN"))){
+                    Comment.findByIdAndDelete(req.params.com_id)
+                    .then((comment)=>{
+                        Publication.findByIdAndUpdate(req.params.id, {$pull : {commentList : req.params.com_id}})
+                        .then(()=> res.status(201).json({message :"Comment delete"}))
+                        .catch((error)=> res.status(400).json({message : "Error Removing a comment from a Publication", error : error}))
+                    })
+                    .catch((error)=> res.status(400).json({message : "Error deleting a comment", error :error}))
+                } else {
+                    res.status(403).json({message : "Unauthorized"})
+                }
+            } else {
+                res.status(200).json({message : "This comment doesn't exist"})
+            }
+        })
+        .catch((error)=> res.status(400).json({message : "Error find comment to delete", error : error}))
+    })
+    .catch((error)=> res.status(400).json({message : "Error finding User to check role", error : error}))
+
+
 }
