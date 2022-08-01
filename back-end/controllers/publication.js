995544c0a31d1ec0d8218966cb561056b6533b6a
sismitrii <floguerin156@gmail.com>
2022-08-01 09:06:36 +0200

    ✨ [publications] : getAllPublication and getOnePublication done, need to be rechecked after creating some publications and somme comments

--- back-end/controllers/publication.js
+++ back-end/controllers/publication.js
@@ -2,6 +2,7 @@
 /*------------------------ IMPORT -----------------------------*/
 /*=============================================================*/
 
+const publications = require('../models/publications')
 const Publication = require('../models/publications')
 
 
@@ -10,11 +11,16 @@ const Publication = require('../models/publications')
 /*=============================================================*/
 
 exports.getAllPublication = (req, res, next) => {
-
+    Publication.find()
+    .then((publications)=> res.status(200).json({publications}))
+    .catch((error)=> res.status(400).json({message :"Error find publications", error : error}))
 }
 
 
 exports.getOnePublication = (req, res, next) => {
+    Publication.findById(req.params.id)
+    .then((publication)=> res.status(200).json({publication}))
+    .catch((error)=> res.status(400).json({message :"Error find this publication", error : error}))
 
 }
 
