288f84136d3bc73239484b0a1fc218616397f39a
sismitrii <floguerin156@gmail.com>
2022-08-11 11:06:31 +0200

    🚧 [Header] : Navbar for mobile almost done. Need animation

--- front-end/src/components/NavBar/index.jsx
+++ front-end/src/components/NavBar/index.jsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {faBars, faHouse, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from "@fortawesome/free-regular-svg-icons"
 import styled from "styled-components"
 import colors from "../../utils/styles/colors"
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import Search from "../Search";
 
 const MenuBars = styled(FontAwesomeIcon)`
     color : ${colors.primary};
@@ -11,16 +14,86 @@ const MenuBars = styled(FontAwesomeIcon)`
     cursor : pointer;
 `
 
+const VerticalMenu = styled.ul`
+    position : absolute;
+    left : 0;
+    top : 125px;
+    width : 100%;
+    list-style-type : none;
+`
+
+
+
+const VerticalMenuElement = styled.li`
+    position : relative;
+    width : 100%;
+    background-color : ${colors.secondary};
+    text-align : center;
+    margin-top : 1px;
+    font-size : 16px;
+    font-weight : 500;
+    transition: all 250ms ease-in-out;
+
+    ${(props)=> 
+       props.isOpen ? "top : 0; opacity: 1; display: block;" : "top : -50px; opacity : 0; display : none;"
+    }
+
+    &:hover {
+        background-color : ${colors.primary};
+    }
+
+    a {
+        display : block;
+        padding : 15px;
+        width : 100%;
+    }
+
+    svg {
+        position : absolute;
+        left : 20px ;
+        font-size : 25px;
+    }
+
+`
+//const translate = 50;
+
+// function translateLi(){
+//     let toReturn ="";
+//     for (let i= 1; i<6;i++){
+//         toReturn += `&:nth-child(${i}){
+//             transform : translateY(${(translate)*i}px);
+//             display : block;
+//             opacity : 1;
+//         }`
+//     }
+//     return toReturn;
+// }
+
+
 
 
 
 function NavBar(props){
 
     const [isOpen, setIsOpen] = useState(false)
-    // faire un ul avec 6 li en position absolute qui ont une width de 100% et une height de 40px
     return (
     <nav>
-    {props.$forMobile ? <MenuBars icon={faBars}/>: <div>NavBar animated</div> }
+    {props.$forMobile ?
+    <>
+    <MenuBars icon={faBars} onClick={()=> setIsOpen(!isOpen)}/>
+    <VerticalMenu>
+        <VerticalMenuElement isOpen={isOpen}><Link to="/Home"><FontAwesomeIcon icon={faHouse} />Accueil</Link></VerticalMenuElement>
+        <VerticalMenuElement isOpen={isOpen}><Link to="/Home"><FontAwesomeIcon icon={faUser} />Profil</Link></VerticalMenuElement>
+        <VerticalMenuElement isOpen={isOpen}><Link to="/Home"><FontAwesomeIcon icon={faGear} />Paramètres</Link></VerticalMenuElement>
+        {/* Peut-etre remplacer le contenu des 2 dernier <li> par <Search> et <Logout>
+         et il renverront sous la taille mobile les icones et le contenu en gardant leur fonctionnalité */}
+        <VerticalMenuElement isOpen={isOpen}><Search/></VerticalMenuElement> 
+        <VerticalMenuElement isOpen={isOpen}><Link to="/Home">Se Déconnecter</Link></VerticalMenuElement>
+    </VerticalMenu>
+    </> 
+    
+    :
+     <div>NavBar animated</div> }
     </nav>)
 }
 
--- front-end/src/components/Search/index.jsx
+++ front-end/src/components/Search/index.jsx
@@ -1,5 +1,49 @@
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+import styled from "styled-components"
+
+const SearchContainer = styled.div`
+    width: 30%;
+    display: flex;
+    justify-content : flex-end;
+    border: 1px solid black;
+`
+
+const SearchIconContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #DDDDDD;
+    font-size: 22px;
+    color: #9A9A9A;
+    cursor: pointer;
+`
+
+
 function Search(){
-    return <div>Search</div>
+    const [isSearching, setIsSearching] = useState(false)
+    return (
+    <>
+    {window.matchMedia("(max-width:768px)").matches ? 
+    <Link to="/Home"><FontAwesomeIcon icon={faMagnifyingGlass} />Recherchez</Link>
+    :
+    <SearchContainer>
+        {isSearching ? 
+        <form>
+            <input type="text" id="search-bar" name="search-bar"/>
+            <button><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        </form>
+        :
+        <SearchIconContainer>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </SearchIconContainer>}
+    </SearchContainer> }
+    </>)
 }
 
 export default Search
