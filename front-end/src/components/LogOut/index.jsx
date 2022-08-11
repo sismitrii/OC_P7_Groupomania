114035662e09d47903bdd1b0dbc457a4481c62bf
sismitrii <floguerin156@gmail.com>
2022-08-11 13:22:29 +0200

    ✨ [LogOut] : component done

--- front-end/src/components/LogOut/index.jsx
+++ front-end/src/components/LogOut/index.jsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import colors from "../../utils/styles/colors"
+import { useNavigate } from "react-router-dom";
+
+
+const LogOutContainer = styled.button`
+    padding : 10px 30px;
+    margin : 0 10px;
+    text-align : center;
+    border : none;
+    border-radius : 12px;
+    outline : none;
+    background-color : ${colors.secondary};
+    cursor : pointer;
+`
+
 function LogOut(){
-    return <div>LogOut</div>
+    const navigate = useNavigate();
+    function handleLogOut(){
+        localStorage.clear();
+        navigate('/login');
+    }
+
+    return (
+        <div onClick={()=>handleLogOut()}>
+            {window.matchMedia("(max-width:768px)").matches ?
+                <Link to="/Home">Se Déconnecter</Link> 
+            : 
+                <LogOutContainer>Deconnection</LogOutContainer>}
+        </div>)
 }
 
 export default LogOut
