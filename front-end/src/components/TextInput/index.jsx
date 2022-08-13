ff87f8e8fd887b1c19c0ef48971e42524df80189
sismitrii <floguerin156@gmail.com>
2022-08-13 14:14:26 +0200

    ✨ [TextInput] : textarea that resize with it content

---
+++ front-end/src/components/TextInput/index.jsx
@@ -0,0 +1,56 @@
+/*====================================================*/
+/* --------------------- Import ----------------------*/
+/*====================================================*/
+import { useState } from "react";
+import styled from "styled-components"
+
+/*====================================================*/
+/* --------------------- Style ----------------------*/
+/*====================================================*/
+
+const StyledInput = styled.textarea`
+    width: 100%;
+    height: ${(props) => props.textHeight};
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    resize: none;
+    font-family: 'Arial';
+    padding: 10px;
+
+    @media (min-width: 768px){
+        font-size: 18px;
+    }
+
+    &::-webkit-scrollbar{
+        width: 0px;
+    }
+`
+
+
+/*====================================================*/
+/* ------------------ Main Function ------------------*/
+/*====================================================*/
+
+function TextInput(props){
+    const [textHeight, setTextHeight] = useState("59px")
+
+    async function handleKeyUp(e){
+        await setTextHeight("auto")
+        let scrollHeight = e.target.scrollHeight;
+        await setTextHeight(scrollHeight+"px");
+    }
+
+    return (
+        <StyledInput
+            onKeyUp={(e)=>handleKeyUp(e)}
+            textHeight = {textHeight}
+            maxLength={500}
+            name={props.input.name} 
+            aria-label={props.input.placeholder} 
+            placeholder={props.input.placeholder}
+         />
+    )
+}
+
+export default TextInput
