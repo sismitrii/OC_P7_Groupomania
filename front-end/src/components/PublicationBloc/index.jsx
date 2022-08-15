8dfd32f4cdf1ba722f856f114249f125655923d6
sismitrii <floguerin156@gmail.com>
2022-08-15 19:40:31 +0200

    🚧 [PublicationBloc] : add switch for different case and publicationBloc to work on design

--- front-end/src/components/PublicationBloc/index.jsx
+++ front-end/src/components/PublicationBloc/index.jsx
@@ -4,16 +4,18 @@
 import styled from "styled-components"
 
 import AddNewPublication from "../AddNewPublication"
+import ShowPublication from "../ShowPublication";
 
 /*====================================================*/
 /* --------------------- Style ----------------------*/
 /*====================================================*/
 
-const Container = styled.div`
+const Container = styled.section`
     width: 100%;
     background-color: #EDEDED;
     border-radius: 15px;
     box-shadow: 2px 6px 8px #C9C9C9;
+    margin-bottom: 30px;
 `
 
 
@@ -21,9 +23,23 @@ const Container = styled.div`
 /* ------------------ Main Function ------------------*/
 /*====================================================*/
 function PublicationBloc(props){
+
+    let toReturn; 
+
+    switch (props.type){
+        case 'add':
+            toReturn = <AddNewPublication />
+            break;
+        case 'show' : 
+            toReturn = <ShowPublication />
+            break
+        default:
+            <></>
+    }
+
     return (
     <Container>
-        <AddNewPublication />
+        {toReturn}
     </Container>)
 }
 
