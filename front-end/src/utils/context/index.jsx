0437152ae7d26df1cf23b3c7e035d830909ac57d
sismitrii <floguerin156@gmail.com>
2022-08-15 16:43:56 +0200

    🐛 [context] : bug spelling connection

--- front-end/src/utils/context/index.jsx
+++ front-end/src/utils/context/index.jsx
@@ -6,24 +6,24 @@ export function ConnectionProvider({children}){
     const init = JSON.parse(localStorage.getItem("dataConnection"))
     let initState = init ? init: {} ;
 
-    const [dataConnexion, setDataConnection] = useState(initState) 
+    const [dataConnection, setDataConnection] = useState(initState)
 
 
     useEffect(()=> {
         const data = JSON.parse(localStorage.getItem("dataConnection"))
         if (data){
-            setDataConnection(data)
+            setDataConnection(data);
         }
     }, [])
 
     useEffect(()=>{
-        localStorage.setItem("dataConnection", JSON.stringify(dataConnexion))
-    },[dataConnexion])
+        localStorage.setItem("dataConnection", JSON.stringify(dataConnection))
+    },[dataConnection])
 
 
 
 
-    return (<ConnectionContext.Provider value={{dataConnexion, setDataConnection}}>
+    return (<ConnectionContext.Provider value={{dataConnection, setDataConnection}}>
         {children}
     </ConnectionContext.Provider>)
 }
