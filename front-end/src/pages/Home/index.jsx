3bb0ac14c06840b1e08d0290962797835ac61696
sismitrii <floguerin156@gmail.com>
2022-08-17 10:18:30 +0200

    🩹 [Home] : useCallback, remove useless code

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,10 +1,8 @@
 /*====================================================*/
 /* ------------------- Import ---------------------*/
 /*====================================================*/
-import { useState, useEffect, useRef, useCallback } from "react"
+import { useState, useEffect, useCallback } from "react"
 import styled from "styled-components"
-
-import useFetch from "../../utils/hooks"
 
 import Header from "../../components/Header"
 import PublicationBloc from "../../components/PublicationBloc"
@@ -30,10 +28,6 @@ const HomeTitle = styled.h1`
     margin: 20px;
 `
 
-const Test = styled.div`
-    margin: 50px;
-`
-
 /*====================================================*/
 /* ------------------- Component ---------------------*/
 /*====================================================*/
@@ -43,7 +37,7 @@ function Home(){
     const [publications, setPublications] = useState([])
     let offset = 0
 
-    const loadMorePublication = async() => {
+    const loadMorePublication = useCallback(async() => {
         try {
             const res = await fetch(`http://localhost:3000/api/publication/${offset}`)
             const dataToAdd = await res.json()
@@ -52,7 +46,7 @@ function Home(){
         } catch (error) {
             console.error(error);
         }
-    }
+    },[setPublications])
 
     const handleScroll = (e)=>{
         if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight){
@@ -66,31 +60,6 @@ function Home(){
     },[])
 
 
-    // on veut charger les data au chargement de la page avec les 5 premieres publications
-    // quand on arrive en bas on ne veut pas re-render tout Home mais juste ajouter les 
-    // const handleObserver = useCallback((entries) => {
-    //     const target = entries[0];
-    //     //Retourne une valeur booléenne valant true si 
-    //     //l'élément cible s'entrecoupe avec la zone d'intersection de l'élément racine
-    //     if (target.isIntersecting) {
-    //         console.log("ref");
-    //         setStartToLoad((prev) => prev + 5); 
-    //         // on update startToLoad donc on a re-render de Home
-    //         // et fetchData du useEffect du hook qui
-            
-    //     }
-    //   }, []);
-
-    // // re-render chaque fois que l'on appelle la callBack donc que l'on arrive en bas ? 
-    // useEffect(() => {
-    //     // https://developer.mozilla.org/fr/docs/Web/API/Intersection_Observer_API#cr%C3%A9ation_dun_observateur_dintersection
-    //     // on crée un IntersectionObserver qui prend en parametre une callBack et ici l'option de marge au top
-    //     const observer = new IntersectionObserver(handleObserver, {root: null, rootMargin: "20px", threshold: 0});
-    //     if (endOfPage.current){
-    //         observer.observe(endOfPage.current);
-    //     }
-    //   }, [handleObserver]);
-
     return (
     <>
         <Header active={"home"}/>
@@ -100,15 +69,7 @@ function Home(){
             {publications.map((publication,i)=>(
                 <PublicationBloc key={i} publication={publication} type={"show"}/>
             ))}
-            {/* {isLoading ?
-            <h1>Loading</h1>
-            :
-            <>
-            <PublicationBloc type={"add"}/>
-                {data[0] && data[0].publicationToReturn.map((publication)=>(
-                    <PublicationBloc key={publication._id} publication={publication} type={"show"}/>
-                ))}
-            </>} */}
+
             
         </Container>
         
