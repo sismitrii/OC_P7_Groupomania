1bffd5ab43960085cd8b656ad36f78bc59f80003
sismitrii <floguerin156@gmail.com>
2022-08-17 16:40:58 +0200

    💫 [Home] add animation isLoading

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -2,10 +2,11 @@
 /* ------------------- Import ---------------------*/
 /*====================================================*/
 import { useState, useEffect, useCallback } from "react"
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
 
 import Header from "../../components/Header"
 import PublicationBloc from "../../components/PublicationBloc"
+import colors from "../../utils/styles/colors"
 
 
 /*====================================================*/
@@ -28,6 +29,34 @@ const HomeTitle = styled.h1`
     margin: 20px;
 `
 
+const animationLoader = keyframes`
+0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Loader = styled.div`
+display: inline-block;
+width: 80px;
+height: 80px;
+
+&::after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #fff;
+    border-color: ${colors.primary} transparent ${colors.secondary} transparent;
+    animation: ${animationLoader} 1.2s linear infinite;
+  }
+`
+
+
 /*====================================================*/
 /* ------------------- Component ---------------------*/
 /*====================================================*/
@@ -35,22 +64,28 @@ function Home(){
     //const [startToLoad, setStartToLoad] = useState(0);
     //const {data, isLoading, error} = useFetch(`http://localhost:3000/api/publication/${startToLoad}`);
     const [publications, setPublications] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
     let offset = 0
+    console.log('render');
 
     const loadMorePublication = useCallback(async() => {
         try {
+            setIsLoading(true);
             const res = await fetch(`http://localhost:3000/api/publication/${offset}`)
             const dataToAdd = await res.json()
             await setPublications((prevPublication) => [...prevPublication, ...dataToAdd.publicationToReturn])
             offset += 5;
+            // est ce que je ferais pas toute les requetes à la suite ?
+            setIsLoading(false);
         } catch (error) {
             console.error(error);
         }
     },[setPublications])
 
     const handleScroll = (e)=>{
-        if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight){
+        if ((window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) && (isLoading === false)){
             loadMorePublication();
+            console.log("loaded");
         }
     }
 
@@ -66,11 +101,10 @@ function Home(){
         <Container>
             <HomeTitle>Fil d'actualités</HomeTitle>
             <PublicationBloc type={"add"}/>
-            {publications.map((publication,i)=>(
+            { publications && publications.map((publication,i)=>(
                 <PublicationBloc key={i} publication={publication} type={"show"}/>
-            ))}
-
-            
+                ))}
+            {isLoading && <Loader></Loader>}
         </Container>
         
     </>
