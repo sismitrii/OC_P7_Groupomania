b9862274faab21b4afe58272f77b13c027412a07
sismitrii <floguerin156@gmail.com>
2022-08-29 15:59:14 +0200

    🚸 [Error] : Link to home on error page if user is connected he have acces to header

--- front-end/src/pages/Error/index.jsx
+++ front-end/src/pages/Error/index.jsx
@@ -3,32 +3,46 @@
 /*====================================================*/
 import styled from "styled-components";
 
-import Header from "../../components/Header";
+import Logo from '../../assets/icon-left-font.svg'
 import ErrorImg from '../../assets/Error404.png'
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ConnectionContext } from "../../utils/context";
 /*====================================================*/
 /* --------------------- Style -----------------------*/
 /*====================================================*/
 const Container = styled.div`
     width: 100%;
     display: flex;
+    flex-direction: column;
+    align-items: center;
     justify-content: center;
     min-width: 320px;
+
+    a {
+        width: 100%;
+        max-width: 1000px;
+    }
 `
 const StyledImg = styled.img`
     width: 80%;
     max-width: 700px;
 `
+const StyledLogo = styled.img`
+    min-width: 320px;
+    width: 100%;
+    
+`
 /*====================================================*/
 /* ---------------------- Main -----------------------*/
 /*====================================================*/
 function Error(){
+    const {dataConnection} = useContext(ConnectionContext)
     return(
-        <>
-            <Header />
-            <Container>
-                <StyledImg src={ErrorImg} alt="page d'erreur 404" />
-            </Container>
-        </>
+        <Container>
+            {!dataConnection && <Link to='/home'><StyledLogo src={Logo} alt="Logo de Groupomania" /></Link>}
+            <StyledImg src={ErrorImg} alt="page d'erreur 404" />
+        </Container>
     )
 }
 
